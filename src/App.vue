10e6664804a510abe6507ecde39e793499470238
Tara Baskara <tbaskara@starqle.com>
2017-07-31 12:13:44 +0700

    Add list characters

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <add-character></add-character>
+    <list-characters></list-characters>
   </div>
 </template>
 
 <script>
 import addCharacter from './components/add-character.vue'
+import listCharacters from './components/list-characters.vue'
 
 export default {
   components:{
-    'add-character': addCharacter
+    'add-character': addCharacter,
+    'list-characters': listCharacters
   },
   data () {
     return {
---
+++ src/components/list-characters.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="list-characters">
+    <h1>All Characters</h1>
+    <div v-for="character in characters" class="single-character">
+      <h2>{{character.name}}</h2>
+      <h3>House of {{character.house}}</h3>
+      <p>{{character.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      characters: []
+    }
+  },
+  methods: {
+
+  },
+  created (){
+    this.$http.get('http://localhost:3000/characters').then(function(data){
+      console.log(data);
+      this.characters = data.body
+    })
+  }
+}
+</script>
+
+<style>
+#list-characters{
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-character{
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+
+</style>
